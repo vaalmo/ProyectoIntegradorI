22309c39c3674923b85a625a80bf263eb8448466
vaalmo <valentinamoralesvillada17@outlook.com>
2023-08-09 22:55:39 -0500

    formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -8,11 +8,7 @@ def home(request):
     ##return HttpResponse('<h1>Hallo everunyan</h1>')
     ##return render(request, 'home.html')
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm: 
-        movies = Movie.objects.filter(title__icontains=searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm': searchTerm})
 
 def about(request): 
     return render(request, 'about.html')
